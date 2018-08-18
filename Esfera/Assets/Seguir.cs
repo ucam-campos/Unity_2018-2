795994137bf73bb808401380495e7a7f529f23e5
Élisson Michael <elissonmichael@yahoo.com.br>
2018-08-18 12:31:13 -0300

    Adiciona Arquivos Desenvolvidos em Sala

---
+++ Esfera/Assets/Seguir.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seguir : MonoBehaviour {
+	public GameObject jogador;
+	Vector3 distancia;
+
+	void Start () {
+		distancia = gameObject.transform.position - jogador.transform.position;
+	}
+
+	void Update () {
+		gameObject.transform.position = jogador.transform.position + distancia;
+	}
+}
