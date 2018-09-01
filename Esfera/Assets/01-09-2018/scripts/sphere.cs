cfb4f083a080cf2fa5cc0853f587ba944b0665b5
216220003 <216220003@net.ucam>
2018-09-01 10:24:40 -0300

    Adiciona Ramo de Jefferson

---
+++ Esfera/Assets/01-09-2018/scripts/sphere.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphere : MonoBehaviour {
+	public int velocidade = 5;
+	private Rigidbody componenteRb;
+
+	void Start () {
+		componenteRb = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		float x = Input.GetAxis ("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+		Debug.Log (x + " | " + z);
+		Vector3 movimento = new Vector3 (x, 0, z);
+		componenteRb.AddForce(movimento * velocidade);
+	}
+}
