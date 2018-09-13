2a65b91e5381443316fdde1effd39f61db4c2591
EnzoRobaina <enzo.robaina@gmail.com>
2018-09-13 01:00:25 -0300

    Adiciona arquivos do projeto

---
+++ Esfera/Assets/Scripts/Item.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour {
+	Rigidbody componente;
+	public Text pontuacao;
+	static int pontos = 0;
+	
+	void Start () {
+		//pontos = 0;
+		//pontuacao.text = pontos.ToString();
+		componente = GetComponent<Rigidbody>();
+	}
+	
+	private void OnTriggerEnter(Collider outro){
+		if (outro.gameObject.name == "Player"){
+			Debug.Log("Collected");
+			pontos += 1;
+			Debug.Log(pontos);
+			pontuacao.text = pontos.ToString();
+			Destroy(gameObject);
+		}
+    }
+		
+	void Update () {
+
+	}
+
+	void FixedUpdate(){
+		transform.Rotate(0, 0, 2);
+	}
+}
