cb9607766d62f5733e69ad600fe596ae8523782d
Élisson Michael <elissonmichael@yahoo.com.br>
2018-10-20 12:27:51 -0300

    Adiciona Rotação Aleatória e Destruir em Contato

---
+++ Nave/Assets/DestruirContato.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestruirContato : MonoBehaviour {
+	public GameObject explosao;
+
+	void OnTriggerEnter(Collider outroCollider) {
+		Destroy (outroCollider.gameObject);
+		Destroy (this.gameObject);
+		Instantiate (explosao, transform.position, transform.rotation);
+	}
+
+}
---
+++ Nave/Assets/RotacionarAleatoriamente.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionarAleatoriamente : MonoBehaviour {
+	private Rigidbody componenteRb;
+	public int forca = 50;
+
+	void Start () {
+		componenteRb = GetComponent<Rigidbody> ();	
+		componenteRb.AddTorque (Random.insideUnitCircle * forca);
+	}
+}
