25bb1c35144fb22bb4c417df8b2d00c5d7929058
Élisson Michael <elissonmichael@yahoo.com.br>
2018-10-20 09:56:17 -0300

    Cria Prefab do Projétil (Tiro)

---
+++ Nave/Assets/Scripts/Atirar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atirar : MonoBehaviour {
+	private Rigidbody componenteRb;
+	public int velocidade;
+
+	void Start () {
+		componenteRb = GetComponent<Rigidbody> ();
+	}
+	
+	void Update () {
+		
+		componenteRb.velocity = Vector3.forward * velocidade;
+	}
+}
