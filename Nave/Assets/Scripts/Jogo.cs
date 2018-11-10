7cb54c2a2ed596cbb595d61a2e069883fbb52138
Élisson Michael <elissonmichael@yahoo.com.br>
2018-11-10 09:53:14 -0400

    Adiciona Co-Rotina de Chuva de Asteroides

--- Nave/Assets/Scripts/Jogo.cs
+++ Nave/Assets/Scripts/Jogo.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Jogo : MonoBehaviour {
 	public GameObject asteroide;
+	public float frequencia;
 
 	void Start () {
-			
+		StartCoroutine ("Chover");
 	}
 	
-	void Update () {
-		
+	IEnumerator Chover () {
+		while (true) {
+			Vector3 posicao = transform.position;
+			Limite limite = new Limite ();
+			posicao.x = Random.Range (limite.xMin, limite.xMax);
+			Instantiate (asteroide, posicao, transform.rotation);
+			yield return new WaitForSeconds (frequencia);
+		}
 	}
 }
