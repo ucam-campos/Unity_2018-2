b1958a41c20563ccec22ee4e7423e2940912eab6
Élisson Michael <elissonmichael@yahoo.com.br>
2018-11-12 23:30:56 -0600

    Adiciona Script de Destruir por Tempo

---
+++ Nave/Assets/DestruirPorTempo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestruirPorTempo : MonoBehaviour {
+	void Start () {
+		Destroy (this.gameObject, 2.0f);
+	}
+}
