9ab8817311186184fbc45f8c82b6d15ae99bcf5e
EnzoRobaina <enzo.robaina@gmail.com>
2018-11-18 19:42:24 -0200

    Adiciona inclinação ao movimentar

--- Nave/Assets/Scripts/Controlar.cs
+++ Nave/Assets/Scripts/Controlar.cs
@@ -6,6 +6,7 @@ public class Controlar : MonoBehaviour {
 	private int velocidade = 6;
 	private Rigidbody rb;
 	private Limite limite = new Limite();
+	private float inclinacao = 3.2f;
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
@@ -20,6 +21,7 @@ public class Controlar : MonoBehaviour {
 		float movVer = Input.GetAxis("Vertical");
 		Vector3 movimento = new Vector3(movHor, 0, movVer);
 		rb.velocity = (movimento*velocidade);
+		rb.rotation = Quaternion.Euler(rb.velocity.z * inclinacao, 0, rb.velocity.x * -inclinacao);
 		float limitarX = Mathf.Clamp (rb.position.x, limite.xMin, limite.xMax);
 		float limitarZ = Mathf.Clamp (rb.position.z, limite.yMin, limite.yMax);
 		rb.position = new Vector3 (limitarX, 0, limitarZ);
