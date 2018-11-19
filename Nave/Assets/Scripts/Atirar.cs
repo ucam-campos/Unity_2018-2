2d42bc3428a8f70926a149d48448177857634a60
EnzoRobaina <enzo.robaina@gmail.com>
2018-11-18 22:34:08 -0200

    Adiciona tiro triplo e aumenta vida dos asteroides p/ balanceamento

--- Nave/Assets/Scripts/Atirar.cs
+++ Nave/Assets/Scripts/Atirar.cs
@@ -16,6 +16,8 @@ public class Atirar : MonoBehaviour {
 		if ((Input.GetButton ("Fire1") || Input.GetKey(KeyCode.Space)) && Time.time >= proximoDisparo) {
 			proximoDisparo = Time.time + frequencia;
 			Instantiate (tiro, transform);
+			Instantiate (tiro, new Vector3(transform.position.x + .3f, transform.position.y, transform.position.z - .3f), transform.rotation);
+			Instantiate (tiro, new Vector3(transform.position.x - .3f, transform.position.y, transform.position.z - .3f), transform.rotation);
 			som.Play();
 		}
 	}
