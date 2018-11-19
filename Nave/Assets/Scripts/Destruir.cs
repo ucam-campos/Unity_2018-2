5302d55b56e523d2b40adabbfc1ec2554fd8316b
EnzoRobaina <enzo.robaina@gmail.com>
2018-11-18 23:40:37 -0200

    Adiciona HUD com pontuação

--- Nave/Assets/Scripts/Destruir.cs
+++ Nave/Assets/Scripts/Destruir.cs
@@ -1,24 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Destruir : MonoBehaviour {
 	public int vida;
+	public Text pontuacao;
 	public GameObject explosaoAsteroide;
 	public GameObject explosaoPlayer;
 	private Jogo jogo;
+	static int pontos = 0;
 
 	void Start(){
 		jogo = GameObject.FindGameObjectWithTag ("GameController")
 						 .GetComponent<Jogo> ();
+
+		pontuacao = GameObject.FindGameObjectWithTag("Canvas").GetComponent<Text>();
 	}
 
 	void OnTriggerEnter(Collider outro){
 		if(!outro.CompareTag("ColliderLimite")){
 			vida--;
-			Instantiate (explosaoAsteroide, transform.position, transform.rotation);
+			
+			if(gameObject.CompareTag("Boss")){
+				Instantiate (explosaoAsteroide, transform.position, transform.rotation);
+			}
+
 			if(vida == 0){
 				Destroy (this.gameObject);
+				Instantiate (explosaoAsteroide, transform.position, transform.rotation);
+				pontos++;
+				pontuacao.text = "Pontos: "+pontos;
 			}
 		
 			if (outro.CompareTag ("Player")) {
